26dd86e0dd5c2d3d522823b244ac297598b283ba
OfficerHades <skulkid51@gmail.com>
2023-06-02 14:19:55 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mailScheduler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetExcelFile GF = new GetExcelFile();
+            GF.GetDataAndExportExcel();
+
+            SendMail Sm = new SendMail();
+            Sm.SendEmail("joshua.komolafe@optimusbank.com", "Report on Bill Payments");
+        }
+    }
+
+
+}*/
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mailScheduler
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    GetExcelFile GF = new GetExcelFile();
+                    GF.GetDataAndExportExcel();
+
+                    SendMail Sm = new SendMail();
+                    Sm.SendEmail("joshua.komolafe@gmail.com", "DAILY BET9JA REPORT");
+
+                    await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken); 
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An error occurred while executing the worker task.");
+                    await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken); 
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
+
---
+++ ConsoleApp1/SendMail.cs
@@ -0,0 +1,56 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mailScheduler
+{
+    public class SendMail
+    {
+        public void SendEmail(string MailTo, string MailSubject)
+        {
+            try
+            {
+                string AppLocation = "";
+                AppLocation = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+                AppLocation = AppLocation.Replace("file:\\", "");
+                string file = AppLocation + "\\ExcelFiles\\Bet9jaReport.xlsx";
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
+                DateTime previousDay = DateTime.Now.Date.AddDays(-1);
+
+                mail.From = new MailAddress("skulkid51@gmail.com");
+                mail.To.Add(MailTo);
+                List<string> li = new List<string>();
+                li.Add("whitehombre@outlook.com");
+                li.Add("chiamakamushinwa@gmail.com");
+                mail.CC.Add(string.Join(",", li));
+                mail.Bcc.Add(string.Join(",", li));
+                mail.Subject = MailSubject;
+                mail.Body = "Hello Iretioluwa,\n" +
+                    "I hope you are doing well.\n" +
+                    "Please, find attached report for Bet9ja transactions done yesterday " + previousDay;
+
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment(file);
+                mail.Attachments.Add(attachment);
+
+                // Set SMTP server credentials and enable SSL/TLS if required
+                SmtpServer.Credentials = new System.Net.NetworkCredential("skulkid51@gmail.com", "nbjccuqeixvsewde");
+                SmtpServer.EnableSsl = true;
+
+               SmtpServer.Send(mail);
+               Console.WriteLine("Eamil sent successfully");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
